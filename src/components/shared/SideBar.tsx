8159bf16dbfdf4a203f7ca020b6cc4fef6cb1d94
Jari Mesina <jarimesina1234@gmail.com>
2021-11-14 09:37:29 +0800

    wip (#3)

--- src/components/shared/SideBar.tsx
+++ src/components/shared/SideBar.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 
 export const SideBar: React.FC = ({children}) => {
+  const items = [
+    {
+      routeName: 'My Profile'
+    },
+    {
+      routeName: 'View/Find Jobs'
+    },
+    {
+      routeName: 'Create Jobs'
+    },
+    {
+      routeName: 'Edit Jobs'
+    }
+  ]
   return (
   <div className="flex flex-row" style={{ minHeight: '100vh'}}>
     <div className="w-1/6 bg-purple-500 h-screen" style={{ height: 'auto'}}>
       <ul className="text-white">
-        <li> My Profile </li>
-        <li> View/Find Jobs </li>
-        {/* this will have a detail page */}
-        <li> Create Jobs </li>
-        <li> Edit Jobs</li>
+        {
+          items.map((item, index)=> (
+            <li key={index} className="ml-4 mt-4">{item.routeName}</li>
+          ))
+        }
       </ul>
     </div>
     <div className="w-5/6">
